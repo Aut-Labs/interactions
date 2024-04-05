6d1afa38fc3d4ae22e79e771371fe2674a8a8657
Misha <mihanix.eth@gmail.com>
2024-04-05 10:07:11 +0300

    Reapply "interim"
    This reverts commit a2cf5ae2e0280e44335dd2132415b7086a8235c2.

--- src/misc/Resolver.sol
+++ src/misc/Resolver.sol
@@ -1,9 +1,9 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.23;
 
 import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 
-interface IBasicRouter {
+interface IResolver {
     event RouteSaved(
         address indexed operator,
         address indexed service,
@@ -13,57 +13,58 @@ interface IBasicRouter {
 
     function castHandleToKey(string memory handle) external view returns (bytes32);
 
-    function resolveFor(bytes32 key) external view returns (address);
+    function resolve(bytes32 key) external view returns (address);
 
     function createLink(address resource, bytes32 key, string calldata handle) external;
 }
 
-contract BasicRouter is IBasicRouter, AccessControl {
+abstract contract ResolverErrorHelper {
+    error InitialManagerEmptyError();
+    error KeyHandleMissmatchError();
+}
+
+contract Resolver is IResolver, ResolverErrorHelper, AccessControl {
     bytes32 public constant MANAGER_ROLE = keccak256("MANAGER_ROLE");
     bytes32 public constant OPERATOR_ROLE = keccak256("OPERATOR_ROLE");
 
-    bytes32 internal immutable _artefact;
-    mapping(bytes32 => address) internal _routes;
+    bytes32 internal immutable _KEY_PREFIX;
+    mapping(bytes32 key => address resource) internal _routes;
 
     constructor(address initialOperatorManager) {
-        require(initialOperatorManager != address(0));
+        if (initialOperatorManager == address(0)) {
+            revert InitialManagerEmptyError();
+        }
 
         _setRoleAdmin(OPERATOR_ROLE, MANAGER_ROLE);
         _grantRole(MANAGER_ROLE, initialOperatorManager);
         _grantRole(OPERATOR_ROLE, initialOperatorManager);
 
-        _artefact = keccak256(
+        _KEY_PREFIX = keccak256(
             abi.encodePacked(
                 string.concat(
-                    string.concat(__NAMESPACE, __CONTRACT),
-                    string(
-                        abi.encodePacked(
-                            block.chainid,
-                            block.number,
-                            msg.sender,
-                            tx.gasprice
-                        )
-                    )
+                    string.concat(_NAMESPACE_ROOT, ":resolver_v1"),
+                    string(abi.encodePacked(block.chainid, block.number, msg.sender))
                 )
             )
         );
     }
 
-    function resolveFor(bytes32 key) external view returns (address) {
+    function resolve(bytes32 key) external view returns (address) {
         return _routes[key];
     }
 
     function createLink(address resource, bytes32 key, string calldata handle) external {
-        require(key == castHandleToKey(handle));
+        if (key != castHandleToKey(handle)) {
+            revert KeyHandleMissmatchError();
+        }
         _checkRole(OPERATOR_ROLE);
         _routes[key] = resource;
         emit RouteSaved(msg.sender, resource, key, handle);
     }
 
     function castHandleToKey(string memory handle) public view returns (bytes32) {
-        return keccak256(abi.encode(_artefact, handle));
+        return keccak256(abi.encode(_KEY_PREFIX, handle));
     }
 
-    string private constant __NAMESPACE = "urn:autlabs:contracts";
-    string private constant __CONTRACT = "::interactions:router_v1-alpha";
+    string private constant _NAMESPACE_ROOT = "urn:autlabs:contracts:interactions";
 }
