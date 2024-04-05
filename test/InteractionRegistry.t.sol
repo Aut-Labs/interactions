3a32223db98452e05fe639fa3c9d3310d4e8509b
Misha <mihanix.eth@gmail.com>
2024-04-05 15:36:54 +0300

    add tests

---
+++ test/InteractionRegistry.t.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.23;
+
+import {PRBTest} from "@prb/test/src/PRBTest.sol";
+import {console2} from "forge-std/src/console2.sol";
+import {StdCheats} from "forge-std/src/StdCheats.sol";
+
+import {IInteractionRegistry, InteractionRegistry} from "../src/InteractionRegistry.sol";
+
+contract InteractionRegistryTest is PRBTest, StdCheats {
+    IInteractionRegistry internal subj;
+
+    function setUp() public virtual {
+        subj = IInteractionRegistry(address(new InteractionRegistry(address(this))));
+    }
+
+    function testFuzz_registerInteractionId(uint16 chainId, bytes4 functionSelector, address recipient) public virtual {
+        vm.assume(chainId != 0);
+        vm.assume(recipient != address(0));
+        subj.registerInteractionId(chainId, recipient, functionSelector);
+    }
+
+    function testFail_duplicateRegisterInteractionId() public virtual {
+        uint16 chainId = 1;
+        address recipient = address(this);
+        bytes4 functionSelector = 0xffffffff;
+        subj.registerInteractionId(chainId, recipient, functionSelector);
+        subj.registerInteractionId(chainId, recipient, functionSelector);
+    }
+
+    function testFail_invalidChainIdRegisterInteractionId() public virtual {
+        uint16 chainId = 0; // !
+        address recipient = address(this);
+        bytes4 functionSelector = 0xffffffff;
+        subj.registerInteractionId(chainId, recipient, functionSelector);
+    }
+
+    function testFail_invalidRecipientRegisterInteractionId() public virtual {
+        uint16 chainId = 1;
+        address recipient = address(0); // !
+        bytes4 functionSelector = 0xffffffff;
+        subj.registerInteractionId(chainId, recipient, functionSelector);
+    }
+
+    function testFuzz_interactionDataFor(uint16 chainId, address recipient, bytes4 functionSelector) public virtual {
+        vm.assume(chainId != 0);
+        vm.assume(recipient != address(0));
+        subj.registerInteractionId(chainId, recipient, functionSelector);
+        bytes32 interactionId = subj.predictInteractionId(chainId, recipient, functionSelector);
+        assertNotEq(interactionId, bytes32(0));
+        (uint16 dChainId, address dRecipient, bytes4 dFunctionSelector) = subj.interactionDataFor(interactionId);
+        assertEq(dChainId, chainId);
+        assertEq(dRecipient, recipient);
+        assertEq(dFunctionSelector, functionSelector);
+    }
+
+    function testFuzz_AccessControlRegisterInteractionId(address sender) public virtual {
+        vm.assume(sender != address(0));
+        vm.expectRevert();
+        vm.prank(sender);
+        subj.registerInteractionId(1, address(this), 0xffffffff);
+    }
+}
