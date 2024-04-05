cd3e48f21335f769c6ee3df454ee789b12db07ab
Misha <mihanix.eth@gmail.com>
2024-04-05 15:34:48 +0300

    add some docs

--- src/misc/Resolver.sol
+++ src/misc/Resolver.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.23;
 
 import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 
+/// @title A contract that contains named addresses of other contracts within its local group
 interface IResolver {
     event RouteSaved(
         address indexed operator,
@@ -11,10 +12,20 @@ interface IResolver {
         string handle
     );
 
-    function castHandleToKey(string memory handle) external view returns (bytes32);
+    /// @dev Conversion from human-readable string handle into 32-byte discovery key
+    /// @param handle human-readable string containing a handle for some relevant contract 
+    /// @return key 32-byte discovery key
+    function castHandleToKey(string memory handle) external view returns (bytes32 key);
 
-    function resolve(bytes32 key) external view returns (address);
+    /// @dev Resolve actual contract address for a given discovery key
+    /// @param key a 32-byte discovery key
+    /// @return resource a smart contract address mapped with the key 
+    function resolve(bytes32 key) external view returns (address resource);
 
+    /// @dev Link human-readable string handle w/ respective discovery key to a contract address
+    /// @param resource smart contract address to register
+    /// @param key 32-byte discovery key for that contract
+    /// @param handle human-readable string handle for that contract (should match `key`)
     function createLink(address resource, bytes32 key, string calldata handle) external;
 }
 
@@ -23,6 +34,7 @@ abstract contract ResolverErrorHelper {
     error KeyHandleMissmatchError();
 }
 
+/// @inheritdoc IResolver
 contract Resolver is IResolver, ResolverErrorHelper, AccessControl {
     bytes32 public constant MANAGER_ROLE = keccak256("MANAGER_ROLE");
     bytes32 public constant OPERATOR_ROLE = keccak256("OPERATOR_ROLE");
@@ -49,10 +61,12 @@ contract Resolver is IResolver, ResolverErrorHelper, AccessControl {
         );
     }
 
+    /// @inheritdoc IResolver
     function resolve(bytes32 key) external view returns (address) {
         return _routes[key];
     }
 
+    /// @inheritdoc IResolver
     function createLink(address resource, bytes32 key, string calldata handle) external {
         if (key != castHandleToKey(handle)) {
             revert KeyHandleMissmatchError();
@@ -62,6 +76,7 @@ contract Resolver is IResolver, ResolverErrorHelper, AccessControl {
         emit RouteSaved(msg.sender, resource, key, handle);
     }
 
+    /// @inheritdoc IResolver
     function castHandleToKey(string memory handle) public view returns (bytes32) {
         return keccak256(abi.encode(_KEY_PREFIX, handle));
     }
